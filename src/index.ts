d7a452e99ffe8d804f8c35561ef4c228ce28d3c0
pranavraagz <pranavraagz@gmail.com>
2021-10-21 20:03:45 +0530

    feat: add cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import * as dotenv from "dotenv";
 import { createConnection } from "typeorm";
 import { router } from "./routes/index";
@@ -15,6 +16,7 @@ createConnection()
     const app = express();
 
     app.use(express.json());
+    app.use(cors);
 
     app.use("/api", router);
 
