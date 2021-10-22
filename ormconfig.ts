4a1129789a9486758cc1dfed586d6e09541cd742
pranavraagz <pranavraagz@gmail.com>
2021-10-22 10:59:32 +0530

    refactor: ormconfig.json -> ormconfig.ts
    Now ormconfig is fetched from .env to make working on multiple devices convenient

---
+++ ormconfig.ts
@@ -0,0 +1,16 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+export = {
+  type: process.env.DB_TYPE,
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: false,
+  entities: ["src/entity/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: ["src/subscriber/**/*.ts"],
+};
